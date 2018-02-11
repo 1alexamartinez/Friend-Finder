b6899cd48dc425695ff2ae587ad7a3a384a23c17
1alexamartinez <1alexamartinez@gmail.com>
2018-02-10 22:14:49 -0500

    Final Commit

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+let friends = [
+    {
+      "name":"A Poodle!",
+      "photo":"http://www.poodlesinneed.com/siteimages/P1011090.JPG",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          3
+        ] 
+    },
+    {
+      "name":"A Golden Retriever!",
+      "photo":"https://gbgrr.org/wp-content/uploads/Home-page-donate.jpg",
+      "scores":[
+          3,
+          2,
+          5,
+          1,
+          2
+        ]
+    },
+    {
+      "name":"An Irish Setter!",
+      "photo":"https://vetstreet.brightspotcdn.com/dims4/default/7f01c1b/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F63%2F282c60a75911e0a0d50050568d634f%2Ffile%2FIrish-Setter-2-645mk062411.jpg",
+      "scores":[
+          2,
+          3,
+          4,
+          4,
+          3
+        ]
+    },
+    {
+      "name":"A Corgi!",
+      "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2016-10/12/14/asset/buzzfeed-prod-fastlane01/sub-buzz-11888-1476296433-5.png?downsize=715:*&output-format=auto&output-quality=auto",
+      "scores":[
+          5,
+          5,
+          5,
+          5,
+          4
+        ]
+    },
+    {
+      "name":"A Greyhound!",
+      "photo":"http://www.greyhoundcrossroads.com/uploads/Proper%20Weight/Wiki%20at%2071lb%20race%20weight.jpg?1496346765432",
+      "scores":[
+          2,
+          3,
+          4,
+          5,
+          1
+        ]
+    },
+    {
+      "name":"An Old English Sheepdog",
+      "photo":"https://a-z-animals.com/media/animals/images/original/old_english_sheepdog2.jpg",
+      "scores":[
+          4,
+          2,
+          3,
+          1,
+          4
+        ]
+    }
+    ];
+    
+    module.exports = friends;
